f12bb08119679dd6df5c941b331d17321796ed80
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2018-01-15 14:31:00 +0100

    Add project files.

---
+++ SwiftPM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SwiftPM.Startup))]
+namespace SwiftPM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
